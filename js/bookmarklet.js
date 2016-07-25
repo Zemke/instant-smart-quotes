61dbb05e1821f4d7f3e56b8b09e47193b8beee71
Zemke <flzemke@gmail.com>
2016-07-25 22:40:07 +0200

    Init version

---
+++ js/bookmarklet.js
@@ -0,0 +1,135 @@
+/**
+ * Smart Quotes by Florian Zemke, regex by manki.in
+ * https://github.com/Zemke/smart-quotes
+ *
+ * Replace typewriter quotes, apostrophes, ellipses and dashes
+ * with their typographically correct counterparts as you type.
+ *
+ * Wrap in braces {{"The brace's chars will be ignored."}} to ignore.
+ */
+document.addEventListener('input', function () {
+
+  /**
+   * Will return a boolean value based on
+   * whether or not the HTML element is a text field or not.
+   *
+   * @param {object} activeElement An HTML Element
+   * @returns {bool} True, if the element is of type
+   *  `TEXTAREA` or `INPUT` with type `text`, false otherwise.
+   */
+  var isTextField = function (activeElement) {
+    if (activeElement.tagName.toUpperCase() == 'TEXTAREA'
+      || (activeElement.tagName.toUpperCase() == 'INPUT'
+      && activeElement.type.toUpperCase() == 'TEXT'))   {
+      return true;
+    }
+    return false;
+  };
+
+  /**
+   * Takes a text field and counts the characters from
+   * the position of the caret until the end of the string.
+   *
+   * @param {object} activeElement Either a textarea or an input element
+   *  of type text.
+   * @returns {Number} The number of characters until the end of the string.
+   */
+  var charsTillEndOfStr = function (activeElement) {
+    return activeElement.value.length - activeElement.selectionStart;
+  };
+
+  /**
+   * Determines the correct position of the caret and sets it.
+   * Used to keep the caret at the position of where
+   * it was before regex changed the string.
+   *
+   * @param {object} activeElement Either a textarea or an input element
+   *  of type text.
+   * @param {Number} charsTillEndOfStr See {@link charsTillEndOfStr}.
+   * @returns {Number} The correct position of the caret.
+   */
+  var correctCaretPosition = function (activeElement, charsTillEndOfStr) {
+    var correctCaretPos = activeElement.value.length - charsTillEndOfStr;
+    activeElement.selectionStart, activeElement.selectionEnd = correctCaretPos;
+    return correctCaretPos;
+  };
+
+  /**
+   * Do the job of taking the appropriate steps to replace the string.
+   *
+   * @param {Object} activeElement Either a textarea or an input element
+   *  of type text.
+   * @return {String} The new value of the text area.
+   */
+  var processTextField = function (activeElement) {
+    var charsTillEnfOfStrBeforeRegex = charsTillEndOfStr(activeElement);
+    activeElement.value = replaceTypewriterPunctuation(activeElement.value);
+    correctCaretPosition(activeElement, charsTillEnfOfStrBeforeRegex);
+    return activeElement.value;
+  };
+
+  /**
+   * This method will replace quotes, apostrophes, ellipses and dashes
+   * with their typograhpically correct counterparts.
+   *
+   * @param {String} g The text to be replaced.
+   * @returns {String} The text after quotes, apostrophes, ellipses and dashes
+   *  have been replaced.
+   */
+  var replaceTypewriterPunctuation = function (g) {
+    var splitterRegex = /\{{2}[\S\s]*?\}{2}/g;
+    var f = false,
+      d = "",
+      h = g.split(splitterRegex);
+    if (h.length === 1) {
+      d = regex(g);
+    } else {
+      var a = g.match(splitterRegex);
+      if (!h[0]) {
+        h.shift();
+        f = true;
+      }
+      for (var b = 0; b < h.length; ++b) {
+        var c = regex(h[b]);
+        if (f) {
+          d += a[b] != null ? a[b] + c : c;
+        } else {
+          d += a[b] != null ? c + a[b] : c;
+        }
+      }
+    }
+    return d;
+  };
+
+  /**
+   * This is doing the actual replacements using regex.
+   *
+   * @param {String} g The string to do the replacements with.
+   * @returns {String} The string with the replaced smart punctuation.
+   */
+  var regex = function (g) {
+    return g
+      .replace(/(\s|^|\(|\>|\])(")(?=[^>\]]*(<|\[|$))/g, "$1“")
+      .replace(/(\s|^|\(|\>|\])(')(?=[^>\]]*(<|\[|$))/g, "$1‘")
+      .replace(/([^0-9])(")(?=[^>\]]*(<|\[|$))/g, "$1”")
+      .replace(/([^0-9])(')(?=[^>\]]*(<|\[|$))/g, "$1’")
+      .replace(/(\w|\s)-{3}(\w|\s)/g, "$1—$2")
+      .replace(/(\w|\s)-{2}(\w|\s)/g, "$1–$2")
+      .replace(/([^\\\.…])\.{3}([^\.…])/g, "$1…$2");
+  };
+
+  /**
+   * The starting method.
+   *
+   * @returns {Boolean|String} False, if not called from the command line,
+   *  the new typographically correct string otherwise.
+   */
+  var main = function () {
+    var activeElement = document.activeElement;
+    if (!isTextField(activeElement)) {
+      console.log('Active element is not a text field.');
+      return false;
+    }
+    return processTextField(activeElement);
+  }; main();
+});
