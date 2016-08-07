2797848e9f0224b14d5cc004699ecb50ed6c37e0
Zemke <flzemke@gmail.com>
2016-08-07 12:09:08 +0200

    Add on/off trigger (only visual so far)

--- js/background.js
+++ js/background.js
@@ -1,3 +1,24 @@
+var BADGE = {
+  ON: {
+    TEXT: 'ON',
+    COLOR: '#00A850'
+
+  },
+  OFF: {
+    TEXT: 'OFF',
+    COLOR: '#DBC200'
+  }
+};
+
 chrome.browserAction.onClicked.addListener(function(tab) {
-    chrome.tabs.executeScript(tab.id, {file: "js/bookmarklet.js"})
+  chrome.tabs.executeScript(tab.id, {file: "js/bookmarklet.js"});
+
+  chrome.browserAction.getBadgeText({tabId: tab.id}, function (badgeText) {
+    var newBadge = badgeText === BADGE.ON.TEXT ? BADGE.OFF : BADGE.ON;
+    chrome.browserAction.setBadgeText({text: newBadge.TEXT, tabId: tab.id});
+    chrome.browserAction.setBadgeBackgroundColor({color: newBadge.COLOR, tabId: tab.id});
+  });
 });
+
+chrome.browserAction.setBadgeText({text: BADGE.ON.TEXT});
+chrome.browserAction.setBadgeBackgroundColor({color: BADGE.ON.COLOR});
