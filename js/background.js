e4bcea860417239738bb994eff66f2787347455e
Zemke <flzemke@gmail.com>
2016-08-15 15:20:33 +0200

    Restructure background file and send message to content script

--- js/background.js
+++ js/background.js
@@ -2,16 +2,16 @@ var BADGE = {
   ON:  {TEXT: 'ON',  COLOR: '#00A850'},
   OFF: {TEXT: 'OFF', COLOR: '#6B0031'}
 };
+var currentBadge = BADGE.ON;
 
-chrome.browserAction.onClicked.addListener(function(tab) {
-  chrome.tabs.executeScript(tab.id, {file: "js/bookmarklet.js"});
+chrome.browserAction.setBadgeText({text: currentBadge.TEXT});
+chrome.browserAction.setBadgeBackgroundColor({color: currentBadge.COLOR});
 
-  chrome.browserAction.getBadgeText({tabId: tab.id}, function (badgeText) {
-    var newBadge = badgeText === BADGE.ON.TEXT ? BADGE.OFF : BADGE.ON;
-    chrome.browserAction.setBadgeText({text: newBadge.TEXT, tabId: tab.id});
-    chrome.browserAction.setBadgeBackgroundColor({color: newBadge.COLOR, tabId: tab.id});
+chrome.browserAction.onClicked.addListener(function (tab) {
+  currentBadge = currentBadge === BADGE.ON ? BADGE.OFF : BADGE.ON;
+
+  chrome.tabs.sendMessage(tab.id, {isEnabled: (currentBadge === BADGE.ON)}, function (response) {
+    chrome.browserAction.setBadgeText({text: currentBadge.TEXT, tabId: tab.id});
+    chrome.browserAction.setBadgeBackgroundColor({color: currentBadge.COLOR, tabId: tab.id});
   });
 });
-
-chrome.browserAction.setBadgeText({text: BADGE.ON.TEXT});
-chrome.browserAction.setBadgeBackgroundColor({color: BADGE.ON.COLOR});
